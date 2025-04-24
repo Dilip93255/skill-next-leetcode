4312d8ba04607ad040e5ab77d52917b13980fe25
P.DILIP REDDY <pullanagaridilipreddy@gmail.com>
2025-04-24 10:09:33 +0530

    Time: 4 ms (36.50%) | Memory: 43.8 MB (59.61%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,49 @@
+/*class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> Stack = new Stack<>();
+
+        for(int num : nums2){
+            while(!stack.isEmpty() && num > stack.peek()) {
+                int smallerNum = stack.pop();
+                nextGreaterMap.put(smallerNum, num);
+            }
+            stack.push(num);
+        }
+        while (!stack.isEmpty()) {
+            nextGreaterMap.put(stack.pop(), -1);
+        }
+        int[] result = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            result[i] = nextGreaterMap.get(nums1[i]);
+        }
+
+    
+*/
+
+
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                int smallerNum = stack.pop();
+                nextGreaterMap.put(smallerNum, num);
+            }
+            stack.push(num);
+        }
+
+    
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
